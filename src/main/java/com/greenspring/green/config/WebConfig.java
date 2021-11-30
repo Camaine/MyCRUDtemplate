25ce4a12431cce6298ceb5de2c031a12c69dc8d7
Juwon Kim <kang9290@gmail.com>
2021-12-01 00:42:10 +0900

    next js 테스트 반영

--- src/main/java/com/greenspring/green/config/WebConfig.java
+++ src/main/java/com/greenspring/green/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000","http://greenworld.dscloud.me:3000","https://www.furse.io","https://furse.io")
+                .allowedOrigins("http://localhost:3000","http://greenworld.dscloud.me:3000","https://www.furse.io","https://furse.io","https://nextjs.d26sdrbm8fvd8f.amplifyapp.com")
                 .allowedMethods("GET", "POST","PUT","DELETE");
     }
 }
